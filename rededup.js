nickgaya/rededup

commit c4fdcd4e615f482af10008c14cd0d12c0346d6ad

Tweak selector for search results container

rededup.js
@@ -61,9 +61,9 @@ function getPageInfo() {
         };
     } else if (document.body.classList.contains('combined-search-page')) {
         // Combined search page
-        const groups = document.body.querySelectorAll('.search-result-group');
         return {
-            container: groups[groups.length-1],
+            container: document.body.querySelector(
+                '.content > .search-result-listing > .search-result-group'),
             pageType: PageType.SEARCH_PAGE,
         };
     } else if (document.body.classList.contains('search-page')) {

commit b042b0405b61d1aad585e67003c3380cadceb4b4

Fix bug preventing processing of search results (introduced in c4fdcd4)

rededup.js
@@ -63,9 +63,10 @@ function getPageInfo() {
         };
     } else if (document.body.classList.contains('combined-search-page')) {
         // Combined search page
+        const containers = document.body.querySelectorAll(
+            '.search-result-group');
         return {
-            container: document.body.querySelector(
-                '.content > .search-result-listing > .search-result-group'),
+            container: containers[containers.length - 1],
             pageType: PageType.SEARCH_PAGE,
         };
     } else if (document.body.classList.contains('search-page')) {